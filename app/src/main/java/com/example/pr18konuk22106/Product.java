27ff526ebeb7bf21d0dc15e6e0ad81568f184bf1
noitorra <ivankonuk00@gmail.com>
2023-10-16 17:29:39 +0700

    NotWork2

---
+++ app/src/main/java/com/example/pr18konuk22106/Product.java
@@ -0,0 +1,17 @@
+package com.example.pr18konuk22106;
+
+public class Product {
+
+    String name;
+    int price;
+    int image;
+    boolean box;
+
+
+    Product(String _describe, int _price, int _image, boolean _box) {
+        name = _describe;
+        price = _price;
+        image = _image;
+        box = _box;
+    }
+}
